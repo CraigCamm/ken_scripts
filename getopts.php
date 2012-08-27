d6fd3a44fb31261642f7411adc99f66ae3a375cc
Ken Mitchner <ken.mitchner@mongoosemetrics.com>
2012-08-27 16:59:15 -0400

    Created a replacement for getopts

---
+++ getopts.php
@@ -0,0 +1,54 @@
+#!/usr/bin/php -q
+<?php
+
+function getopt_loc($args,$opts) {
+    $options = array();
+
+    // Loop through the options, looking for matching arguments.
+    foreach($opts as $opt) {
+        // Configuration array validation.
+        if(!isset($opt['short']) || !isset($opt['long']) || ( !isset($opt['default']) && !isset($opt['required']) )) return NULL;
+        // NULL if required
+        $options[$opt['long']] = ( isset($opt['required']) && ( $opt['required']===true || $opt['required'] == 'true' ) ) ? NULL : $opt['default'];
+        // Loop through the command line arguments to find the current option.
+        foreach($args as $index=>$arg) {
+            if( preg_match("/^--{$opt['long']}=/",$arg) ) {
+                // --option=value
+                $options[$opt['long']] = preg_replace("/--{$opt['long']}=/","",$arg);
+            } else if( preg_match("/^-{$opt['short']}.+/",$arg) ) {
+                // -oValue    
+                $options[$opt['long']] = preg_replace("/^-{$opt['short']}/","",$arg);
+            } else if( preg_match("/^[-]*{$opt['long']}$/",$arg) || preg_match("/^-{$opt['short']}$/",$arg) ) {
+                // option or -option or --option or -o
+                $options[$opt['long']] = true;
+            }
+        }
+        // Required Value Validation
+        if($options[$opt['long']]===NULL) return NULL;
+    }
+    return $options;
+}
+
+// This is to be overloaded with configuration meta-data.
+// Below is a sample
+$options_configuration = array(
+    'apple' => array(
+        'short' => 'a',
+        'long' => 'apple',
+        'required' => true,
+    ),  
+    'banana' => array(
+        'short' => 'b',
+        'long' => 'banana',
+        'default' => 'peel',
+    ),  
+    'cherry' => array(
+        'short' => 'c',
+        'long' => 'cherry',
+        'default' => false,
+    ),
+);
+
+$opts = getopt_loc($argv,$options_configuration);
+
+var_dump($opts);
