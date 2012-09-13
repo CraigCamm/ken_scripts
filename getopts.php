6ee9c05b2a168d4aa20031283bccaf5db37c0bae
Ken Mitchner <ken.mitchner@mongoosemetrics.com>
2012-09-13 13:14:10 -0400

    csv stuffs

--- getopts.php
+++ getopts.php
@@ -1,17 +1,17 @@
 #!/usr/bin/php -q
 <?php
 
-function getopt_loc($args,$opts) {
+/* Process non-positional arguments */
+function getopt_loc($args,$config) {
     $options = array();
-
     // Loop through the options, looking for matching arguments.
-    foreach($opts as $opt) {
+    foreach($config as $opt) {
         // Configuration array validation.
         if(!isset($opt['short']) || !isset($opt['long']) || ( !isset($opt['default']) && !isset($opt['required']) )) return NULL;
         // NULL if required
         $options[$opt['long']] = ( isset($opt['required']) && ( $opt['required']===true || $opt['required'] == 'true' ) ) ? NULL : $opt['default'];
         // Loop through the command line arguments to find the current option.
-        foreach($args as $index=>$arg) {
+        foreach($args as $arg) {
             if( preg_match("/^--{$opt['long']}=/",$arg) ) {
                 // --option=value
                 $options[$opt['long']] = preg_replace("/--{$opt['long']}=/","",$arg);
@@ -19,10 +19,11 @@ function getopt_loc($args,$opts) {
                 // -oValue    
                 $options[$opt['long']] = preg_replace("/^-{$opt['short']}/","",$arg);
             } else if( preg_match("/^[-]*{$opt['long']}$/",$arg) || preg_match("/^-{$opt['short']}$/",$arg) ) {
-                // option or -option or --option or -o
+                // -option or --option or -o or option
                 $options[$opt['long']] = true;
             }
         }
+        var_dump($options);
         // Required Value Validation
         if($options[$opt['long']]===NULL) return NULL;
     }
